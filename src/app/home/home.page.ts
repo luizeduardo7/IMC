d4f8004759758459d85d564b639afd71a03ef976
luizeduardo7 <luisedu11201184@gmail.com>
2022-12-11 21:32:59 -0300

    IMC com mensagem

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,7 @@ export class HomePage {
   weight: number = 0
   height: number = 0
   imc: number = 0
+  msg: string = ""
 
   constructor(private toastCtrl: ToastController) {}
 
@@ -18,18 +19,31 @@ export class HomePage {
     if (this.height <= 0 || this.weight <= 0) {
       return
     }
-    
+
     this.imc = this.weight / (this.height * this.height)
+    this.onMensage()
     this.showIMC()
   }
-
+  onMensage(){
+    if(this.imc < 18.5 && this.imc >= 0){
+      this.msg = "Magreza"
+    }else if(this.imc >= 18.5 && this.imc <= 24.9){
+      this.msg = "Normal"
+    }else if(this.imc >= 25.0 && this.imc <= 29.9){
+      this.msg = "Sobrepeso"
+    }else if(this.imc >= 30.0 && this.imc <= 39.9){
+      this.msg = "Obesidade"
+    }else{
+      this.msg = "Obesidade Grave"
+    }
+  }
   async showIMC() {
     const toast = await this.toastCtrl.create({
-      message: `IMC = ${this.imc.toFixed(2)}`,
+      message: `IMC = ${this.imc.toFixed(2)} - ${this.msg}`,
       duration: 3000,
       color: 'secondary'
     })
-    
+
     toast.present()
 
   }
